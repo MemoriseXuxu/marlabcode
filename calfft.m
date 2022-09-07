c1820eaf60370994fcbee9b05d728f81a1987089
MemoriseXuxu <48404183+MemoriseXuxu@users.noreply.github.com>
2022-09-07 20:24:44 +0800

    Update calfft.m

--- calfft.m
+++ calfft.m
@@ -1,4 +1,14 @@
 function [f,fft_Amp] = calfft(fs,sample)
+% Basic Knowledge 
+%% Time Domain  %%
+% Sampling Rate (samples/sec)  Fs= 1 / delta t; 
+% Frame Size (second) T = N*delta t ;
+% Block Size N -- the data that you collects,usually it's a lot of data.
+%% Frequency Domain %%
+% Fmax = Fs/2 ; which is determined by the delta T;
+% Frequency Resolution delta f = Fmax /SL;
+% Spectral lines SL = N/2;
+
 
 %%  Demo Code  %%
 % f0 = 40 ; %frequency of the sine wave
